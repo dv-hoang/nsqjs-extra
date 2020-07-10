4c1c810d7971c95390f236fbc207dd72dacac8b2
Hoang Dinh <hoang@littlelives.com>
2020-07-10 09:51:56 +0700

    feat: change function name to support nsqjs function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,12 @@ const { Writer } = require('../index')
 
 test('writer promise publish', async done => {
   const writer = new Writer('127.0.0.1', '4151')
-  const rs = await writer.publish('topic', 'msg').catch(() => false)
+  await writer.connectSync()
+
+  const rs = await writer.publishSync('topic', 'msg').catch(() => false)
   expect(rs).toBe(true)
 
-  await writer.close()
+  await writer.closeSync()
+
   done()
 })
